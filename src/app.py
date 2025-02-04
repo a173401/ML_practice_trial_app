05fe637d8befa0864ef9c608fb0e2df744b0f0b9
Бородин А.А. <antoniofagundos@yandex.ru>
2025-02-04 12:50:03 +0300

    - добавил пример .env файла; - добавил считывание параметров из переменных окружения fastapi; - исправил перезапуск в compose файле;

--- src/app.py
+++ src/app.py
@@ -1,7 +1,23 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from functools import lru_cache
+from pydantic import Field
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(env_file='.env', env_file_encoding='utf-8')
+    rabbitmq_password: str = Field(alias="RABBITMQ_USER")
+    rabbitmq_user: str = Field(alias="RABBITMQ_PASSWORD")
+    postgres_user: str = Field(alias="POSTGRES_USER")
+    postgres_password: str = Field(alias="POSTGRES_PASSWORD")
+    postgres_db: str = Field(alias="POSTGRES_DB")
+    redis_password: str = Field(alias="REDIS_PASSWORD")
+
+@lru_cache()
+def get_settings():
+    return Settings()
 
 app = FastAPI()
 
 @app.get("/")
-def read_root():
-    return {"message": "Hello, World!"}
+def read_root(settings = Depends(get_settings)):
+    return {"message": "Hello, World!", "postgres_db": settings.postgres_db}
